30f75ef7c3e698564dbc864542322553e82bd073
Pavel Ivanenko <pivanenko@dataxu.com>
2016-12-26 23:48:40 +0200

    cleanup samurai

--- Tuner/src/main/java/org/tuner/Samurai.java
+++
@@ -1,8 +0,0 @@
-package org.tuner;
-
-public class Samurai {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(" I am Samurai");
-        System.out.println(" args length " + args.length);
-    }
-}
